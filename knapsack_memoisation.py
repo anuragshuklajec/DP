1645f6b5d9d486fd4fd13127ee7e2b11b57be065
anuragshuklajec <anuragshuklajec@gmail.com>
2022-09-09 13:38:24 +0530

    change dp array initialisation

--- knapsack_memoisation.py
+++ knapsack_memoisation.py
@@ -3,18 +3,19 @@ def knapsack_memo(wt,val,W,n,dp):
         return 0
     if dp[n][W] != -1:
         return dp[n][W]
-    else:
-        if wt[n-1] <= W:
-            dp[n][W] = max(val[n-1] + knapsack_memo(wt,val,W-wt[n-1],n-1,dp),knapsack_memo(wt,val,W,n-1,dp))
 
-        else:
-            dp[n][W] = knapsack_memo(wt,val,W,n-1,dp)
+    if wt[n-1] <= W:
+        dp[n][W] = max(val[n-1] + knapsack_memo(wt,val,W-wt[n-1],n-1,dp),knapsack_memo(wt,val,W,n-1,dp))
         return dp[n][W]
+    else:
+        dp[n][W] = knapsack_memo(wt,val,W,n-1,dp)
+        return dp[n][W]
+
 
 
 wt = list(map(int,input().split()))
 val = list(map(int,input().split()))
 W =int(input())
 n = len(wt)
-dp = [[-1 for i in range(n+1)] for j in range(W+1)]
+dp = [[-1 for i in range(W+1)] for j in range(n+1)]
 print(knapsack_memo(wt,val,W,n,dp))
