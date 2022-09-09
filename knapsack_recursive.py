1f4aa219d5000300bf0e396a7dea2aeba940b2e6
anuragshuklajec <anuragshuklajec@gmail.com>
2022-09-09 12:09:59 +0530

    recursive knapsack

--- knapsack_recursive.py
+++ knapsack_recursive.py
@@ -1,13 +1,11 @@
 def knapsack(wt,val,W,n):
-    if len(wt) == 0 or W == 0:
+    if n == 0 or W == 0:
         return 0
 
     if wt[n-1] <= W:
         return max(val[n-1] + knapsack(wt,val,W-wt[n-1],n-1), knapsack(wt,val,W,n-1))
     else:
         return knapsack(wt,val,W,n-1)
-
-
 
 wt = list(map(int,input().split()))
 val = list(map(int,input().split()))
